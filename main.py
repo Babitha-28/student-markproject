b039aa2f1e687b6e7969b41db98091fce42a2cec
Babitha <shettybabitha7@gmail.com>
2026-02-21 12:09:27 +0530

    updated students marks

---
+++ main.py
@@ -0,0 +1,12 @@
+# Students marks analysis project
+
+marks=[85,80,90,70,97]
+print("Marks:",marks)
+total=sum(marks)
+print("Total:",total)
+average=total/len(marks)
+print("Average:",average)
+highest=max(marks)
+print("Highest:",highest)
+lowest=min(marks)
+print("Lowest:",lowest)
